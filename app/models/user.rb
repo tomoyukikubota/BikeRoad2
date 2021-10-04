f93dcaecf9c29418e85e284af533a570d45fb477
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-10-04 07:47:13 +0000

    復習4日目

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
   attachment :profile_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :posts do
     resource :likes, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
 end
