065b125dcba03ea0686ca134114d17124c6361e3
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-10-13 09:31:44 +0000

    復習6日目

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,13 +4,13 @@ class RelationshipsController < ApplicationController
   def create
     current_user.follow(params[:user_id])
     @user = User.find(params[:user_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
     @user = User.find(params[:user_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 #————————フォロー・フォロワー一覧を表示する-————————————
   def followings
